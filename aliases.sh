4094c1f4d6219348e941ace1d2d68e0d3768da78
Wei Zhang <veeamd@gmail.com>
2021-11-19 15:56:17 -0500

    more alias for xcode

--- aliases.sh
+++ aliases.sh
@@ -2,9 +2,13 @@
 
 # remove carthage cache
 alias rmcc='rm -rf ~/Library/Caches/org.carthage.CarthageKit'
+alias rmxd='rm -rf ~/Library/Developer/Xcode/DerivedData'
+alias rmsc='rm -rf ~/Library/Caches/org.swift.swiftpm/'
 
 # carthage bootstrap
 alias cb='carthage bootstrap --platform ios --cache-builds'
+# carthage build
+alias cbd='carthage build --platform ios --no-skip-current'
 
 # xcode-select
 alias xs11='sudo xcode-select -s /Applications/Xcode11.7.app/Contents/Developer'
